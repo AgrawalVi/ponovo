7fa461f4df9414fd5ba7638a2cfdd3022abb4f73
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-17 01:12:00 -0500

    created database schema with drizzle

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,27 +1,138 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
 
-export const usersTable = pgTable('users_table', {
+export const applicationStatusEnum = pgEnum('application_status', [
+  'wishlist',
+  'applied',
+  'rejected',
+  'accepted',
+])
+export const timeLineUpdateEnum = pgEnum('time_line_update', [
+  'applied',
+  'online-assessment',
+  'interview',
+  'rejected',
+  'accepted',
+])
+
+export const users = pgTable('users', {
   id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
   email: text('email').notNull().unique(),
-})
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
 
-export const postsTable = pgTable('posts_table', {
-  id: serial('id').primaryKey(),
-  title: text('title').notNull(),
-  content: text('content').notNull(),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at')
     .notNull()
     .$onUpdate(() => new Date()),
 })
 
-export type InsertUser = typeof usersTable.$inferInsert
-export type SelectUser = typeof usersTable.$inferSelect
+export const usersRelations = relations(users, ({ many }) => ({
+  jobApplications: many(jobApplications),
+  companies: many(companies),
+}))
 
-export type InsertPost = typeof postsTable.$inferInsert
-export type SelectPost = typeof postsTable.$inferSelect
+export const companies = pgTable('companies', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  name: text('name').notNull(),
+  description: text('description'),
+})
+
+export const companiesRelations = relations(companies, ({ one, many }) => ({
+  users: one(users, {
+    fields: [companies.userId],
+    references: [users.id],
+  }),
+  jobApplications: many(jobApplications),
+}))
+
+export const jobApplications = pgTable('job_application', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  applicationStatus: applicationStatusEnum('application_status')
+    .notNull()
+    .default('applied'),
+
+  companyId: integer('company_id'),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+export const jobApplicationsRelations = relations(
+  jobApplications,
+  ({ one, many }) => ({
+    users: one(users, {
+      fields: [jobApplications.userId],
+      references: [users.id],
+    }),
+    jobApplicationTimelineUpdates: many(jobApplicationTimelineUpdates),
+    company: one(companies, {
+      fields: [jobApplications.companyId],
+      references: [companies.id],
+    }),
+  })
+)
+
+export const jobApplicationTimelineUpdates = pgTable(
+  'job_application_timeline_update',
+  {
+    id: serial('id').primaryKey(),
+    jobApplicationId: integer('job_application_id')
+      .notNull()
+      .references(() => jobApplications.id, { onDelete: 'cascade' }),
+    timeLineUpdate: timeLineUpdateEnum('time_line_update').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at')
+      .notNull()
+      .$onUpdate(() => new Date()),
+
+    comments: text('comments'),
+  }
+)
+
+export const jobApplicationTimelineUpdatesRelations = relations(
+  jobApplicationTimelineUpdates,
+  ({ one }) => ({
+    jobApplication: one(jobApplications, {
+      fields: [jobApplicationTimelineUpdates.jobApplicationId],
+      references: [jobApplications.id],
+    }),
+  })
+)
+
+export const jobPosts = pgTable('job_post', {
+  id: serial('id').primaryKey(),
+  companyName: text('company_name').notNull(),
+  jobTitle: text('job_title').notNull(),
+
+  jobDescriptionRichText: text('job_description_rich_text'),
+  jobDescriptionPlainText: text('job_description_plain_text'),
+
+  url: text('url').notNull(),
+
+  jobApplicationId: integer('job_application_id')
+    .notNull()
+    .references(() => jobApplications.id, { onDelete: 'cascade' }),
+})
+
+export const jobPostsRelations = relations(jobPosts, ({ one }) => ({
+  jobApplication: one(jobApplications, {
+    fields: [jobPosts.jobApplicationId],
+    references: [jobApplications.id],
+  }),
+}))
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,10 @@
 import { drizzle } from 'drizzle-orm/neon-http'
 import { neon } from '@neondatabase/serverless'
 import { config } from 'dotenv'
+import * as schema from '@/drizzle/schema'
 
 config({ path: '.env' })
 
 const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle(sql)
+
+export const db = drizzle(sql, { schema })
