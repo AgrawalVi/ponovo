09d3ea80846b566aee61c3446a60681104693787
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-18 17:24:45 -0500

    added sample blob upload

--- src/app/dashboard/applications/page.tsx
+++ src/app/dashboard/applications/page.tsx
@@ -1,22 +1,43 @@
 'use client'
 
-import { UploadButton } from '@/lib/uploadthing'
+import type { PutBlobResult } from '@vercel/blob'
+import { useState, useRef } from 'react'
 
-export default function Home() {
+export default function AvatarUploadPage() {
+  const inputFileRef = useRef<HTMLInputElement>(null)
+  const [blob, setBlob] = useState<PutBlobResult | null>(null)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log('Files: ', res)
-          alert('Upload Completed')
+    <>
+      <h1>Upload Your Avatar</h1>
+
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault()
+
+          if (!inputFileRef.current?.files) {
+            throw new Error('No file selected')
+          }
+
+          const file = inputFileRef.current.files[0]
+
+          const response = await fetch(`/api/upload?filename=${file.name}`, {
+            method: 'POST',
+            body: file,
+          })
+
+          const newBlob = (await response.json()) as PutBlobResult
+
+          setBlob(newBlob)
         }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`)
-        }}
-      />
-    </main>
+      >
+        <input name="file" ref={inputFileRef} type="file" required />
+        <button type="submit">Upload</button>
+      </form>
+      {blob && (
+        <div>
+          Blob url: <a href={blob.url}>{blob.url}</a>
+        </div>
+      )}
+    </>
   )
 }
