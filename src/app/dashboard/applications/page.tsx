bc0435a66318431dfb8ef6f42c3eae99697b766e
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-18 00:59:21 -0500

    initialized uploadthing for file uploads

--- src/app/dashboard/applications/page.tsx
+++ src/app/dashboard/applications/page.tsx
@@ -1,3 +1,22 @@
-export default function Applications() {
-  return <div>Applications</div>
+'use client'
+
+import { UploadButton } from '@/lib/uploadthing'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log('Files: ', res)
+          alert('Upload Completed')
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`)
+        }}
+      />
+    </main>
+  )
 }
