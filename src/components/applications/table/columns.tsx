4d397c8f09f0553a9fa436f32d3496e37fd3911b
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-18 23:56:35 -0500

    added some column actions - do nothing

--- src/components/applications/table/columns.tsx
+++ src/components/applications/table/columns.tsx
@@ -2,6 +2,17 @@
 
 import { ColumnDef } from '@tanstack/react-table'
 import { dbJobApplication } from '@/types'
+import { format } from 'date-fns'
+import { applicationStatusEnum } from '@/types'
+import { DropdownMenu } from '@/components/ui/dropdown-menu'
+import { MessageCirclePlus, PencilIcon, Trash2Icon } from 'lucide-react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import { Button } from '@/components/ui/button'
 
 export const applicationTableColumns: ColumnDef<dbJobApplication>[] = [
   {
@@ -15,9 +26,58 @@ export const applicationTableColumns: ColumnDef<dbJobApplication>[] = [
   {
     accessorKey: 'applicationStatus',
     header: 'Application Status',
+    cell: ({ row }) => {
+      const status = row.getValue('applicationStatus') as applicationStatusEnum
+      return <div className="text-center">{status}</div>
+    },
   },
   {
     accessorKey: 'dateApplied',
     header: 'Date Applied',
+    cell: ({ row }) => {
+      const date = row.getValue('dateApplied') as string
+      return format(new Date(date), 'P')
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const jobApplication = row.original
+
+      return (
+        <div className="space-x-2">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <MessageCirclePlus size="20" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Add Timeline Event</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <PencilIcon size="20" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Edit Information</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="hover:bg-destructive/10 hover:text-destructive"
+                >
+                  <Trash2Icon size="20" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Delete Application</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      )
+    },
   },
 ]
