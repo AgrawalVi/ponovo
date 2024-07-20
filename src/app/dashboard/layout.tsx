6d207a7626970d3ce1de58629f0587c8b790b2dd
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-19 23:25:50 -0500

    initialized react query

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,21 @@
 import Footer from '@/components/general/navigation/footer'
 import ProtectedNav from '@/components/general/navigation/protected-nav'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const queryClient = new QueryClient()
+
   return (
-    <div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
-      <ProtectedNav />
-      {children}
-      <Footer />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
+        <ProtectedNav />
+        {children}
+        <Footer />
+      </div>
+    </QueryClientProvider>
   )
 }
