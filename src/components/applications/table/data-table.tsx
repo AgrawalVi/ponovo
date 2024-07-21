01e4ea8bba426f71997494b06af4ae3e1b825a97
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-20 23:46:33 -0500

    styling changes to table

--- src/components/applications/table/data-table.tsx
+++ src/components/applications/table/data-table.tsx
@@ -27,8 +27,9 @@ import { DataTableFacetedFilter } from '@/components/ui/data-table-faceted-filte
 import { dataTableApplicationStatusOptions } from '@/data'
 import { DataTablePagination } from '@/components/ui/data-table-pagination'
 import NewApplicationButton from '@/components/forms/new-application/new-application-button'
-import FullApplicationView from './full-application-view'
+import FullApplicationView from '../full-application-panel/full-application-view'
 import { dbJobApplication } from '@/types'
+import { cn } from '@/lib/utils'
 
 interface DataTableProps {
   columns: ColumnDef<dbJobApplication>[]
@@ -41,7 +42,9 @@ export function ApplicationDataTable({
   data,
   initialSelectedRowId,
 }: DataTableProps) {
-  const [sorting, setSorting] = useState<SortingState>([])
+  const [sorting, setSorting] = useState<SortingState>([
+    { id: 'companyName', desc: false },
+  ])
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
 
   const table = useReactTable({
@@ -74,89 +77,99 @@ export function ApplicationDataTable({
     },
   )
 
-  useEffect(() => {}, [selectedRow])
+  useEffect(() => {
+    console.log('sorting state', sorting)
+  }, [sorting])
 
   return (
-    <div className="space-y-2">
-      <div className="flex w-full items-center justify-between">
-        <div className="flex flex-1 items-center space-x-2">
-          <Input
-            placeholder="Search Companies..."
-            value={
-              (table.getColumn('companyName')?.getFilterValue() as string) ?? ''
-            }
-            onChange={(event) =>
-              table.getColumn('companyName')?.setFilterValue(event.target.value)
-            }
-            className="max-w-sm"
-          />
-          {table.getColumn('applicationStatus') && (
-            <DataTableFacetedFilter
-              column={table.getColumn('applicationStatus')}
-              title="Application Status"
-              options={dataTableApplicationStatusOptions}
+    <div className="flex space-x-4 p-5">
+      <div className="col-span-2 flex-1 space-y-2">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex flex-1 items-center space-x-2">
+            <Input
+              placeholder="Search Companies..."
+              value={
+                (table.getColumn('companyName')?.getFilterValue() as string) ??
+                ''
+              }
+              onChange={(event) =>
+                table
+                  .getColumn('companyName')
+                  ?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
             />
-          )}
-        </div>
-        <div>
-          <NewApplicationButton>
-            <Button>New Application</Button>
-          </NewApplicationButton>
-        </div>
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
-                  className="cursor-pointer"
-                  onClick={() => setSelectedRow(row.original)}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
+            {table.getColumn('applicationStatus') && (
+              <DataTableFacetedFilter
+                column={table.getColumn('applicationStatus')}
+                title="Application Status"
+                options={dataTableApplicationStatusOptions}
+              />
             )}
-          </TableBody>
-        </Table>
+          </div>
+          <div>
+            <NewApplicationButton>
+              <Button>New Application</Button>
+            </NewApplicationButton>
+          </div>
+        </div>
+        <div className="rounded-md border 2xl:w-[60rem]">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </TableHead>
+                    )
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && 'selected'}
+                    className={cn(
+                      'cursor-pointer',
+                      row.original.id === selectedRow?.id && 'bg-muted',
+                    )}
+                    onClick={() => setSelectedRow(row.original)}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <DataTablePagination table={table} />
       </div>
-      <DataTablePagination table={table} />
       <FullApplicationView applicationId={selectedRow?.id} />
     </div>
   )
