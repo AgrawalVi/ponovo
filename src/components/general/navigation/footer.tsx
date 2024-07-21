a09fc0113fb46afe656a4e3c4c28b17512e599b5
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-20 23:46:10 -0500

    footer changes

--- src/components/general/navigation/footer.tsx
+++ src/components/general/navigation/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="h-20 border-t border-border">
-      <div>Footer</div>
+    <footer className="flex h-20 w-full items-center justify-between border-t border-border px-5 text-muted-foreground">
+      <div>Ponovo</div>
+      <div>© 2024 Vishrut Agrawal</div>
     </footer>
   )
 }
