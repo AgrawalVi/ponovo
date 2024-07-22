8b3e6ce4a96a97c8ec252293a1a8881011e237a5
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-22 01:06:01 -0500

    removed unnecessary imports

--- src/data/charts/application-status-pie-chart.ts
+++ src/data/charts/application-status-pie-chart.ts
@@ -1,8 +1,5 @@
 import 'server-only'
 
-import { db } from '@/lib/db'
-import { jobApplications } from '@/drizzle/schema'
-import { eq } from 'drizzle-orm'
 import { getAllJobApplicationsByUserId } from '../job-applications/get-job-applications'
 
 export default async function getApplicationStatusPieChartData(userId: string) {
