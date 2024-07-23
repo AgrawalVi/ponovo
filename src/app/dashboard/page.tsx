b84ac25b06e3240de86d19e41f7a8c2d096ee571
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-22 23:30:47 -0500

    added text to dashboard home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import ChartGrid from '@/components/dashboard/chart-grid'
+import { Button } from '@/components/ui/button'
 import { users } from '@/drizzle/schema'
 import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs/server'
 import { eq } from 'drizzle-orm'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function DashboardPage() {
@@ -13,12 +15,21 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="flex w-full flex-col items-center">
-      <div className="my-10 bg-gradient-to-b from-foreground from-50% to-neutral-600 bg-clip-text pt-2 text-6xl font-semibold text-transparent dark:to-neutral-400">
+    <div className="flex w-full flex-col items-center space-y-8">
+      <div className="mb-2 mt-10 bg-gradient-to-b from-foreground from-50% to-neutral-600 bg-clip-text pt-2 text-6xl font-semibold text-transparent dark:to-neutral-400">
         Dashboard
       </div>
       <div>
         <ChartGrid />
+      </div>
+      <div className="flex flex-col items-center space-y-2">
+        <div className="text-xl">
+          While waiting for more charts to be added, start logging some
+          applications
+        </div>
+        <Link href="/dashboard/applications">
+          <Button>Log Applications</Button>
+        </Link>
       </div>
     </div>
   )
