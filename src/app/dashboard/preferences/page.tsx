6e2cb539e74009603ba93c1b456a4b0b341408c4
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-24 23:45:31 -0500

    fixed formatting in coming-soon page

--- src/app/dashboard/preferences/page.tsx
+++ src/app/dashboard/preferences/page.tsx
@@ -2,7 +2,7 @@ import UserPreferencesForm from '@/components/forms/user-preference-form'
 
 export default function PreferencesPage() {
   return (
-    <div className="flex w-full flex-col items-center justify-center space-y-4">
+    <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
       {/* <UserPreferencesForm applicationGoal={0} /> */}
       <h1 className="text-5xl font-semibold">Coming Soon</h1>
       <ul className="list-inside list-disc">
