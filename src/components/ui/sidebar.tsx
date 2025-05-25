ca92971a7c95b4dc177dc6c2cb25d6bb81a223df
Vishrut Agrawal <zvishrut@gmail.com>
2025-05-25 15:12:54 -0700

    fixed size of sidebar trigger button

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -296,7 +296,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      <PanelLeft size={16} />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
