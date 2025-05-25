840ff4bd1ef058bdd356d3b827ac4ab4eccbbf5d
Vishrut Agrawal <zvishrut@gmail.com>
2025-05-25 15:41:46 -0700

    added link to home

--- src/components/sidebar-header.tsx
+++ src/components/sidebar-header.tsx
@@ -4,41 +4,32 @@ import * as React from 'react'
 import { ChevronsUpDown, Home, Plus } from 'lucide-react'
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from '@/components/ui/sidebar'
+import Link from 'next/link'
 
 export function TeamSwitcher() {
-  const { isMobile } = useSidebar()
-
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <SidebarMenuButton
-          size="lg"
-          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-        >
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
-            <Home className="size-4 transition-all" />
-          </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">Ponovo</span>
-            {/* <span className="truncate text-xs text-muted-foreground">
+        <Link href="/">
+          <SidebarMenuButton
+            size="lg"
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
+            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
+              <Home className="size-4 transition-all" />
+            </div>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-semibold">Ponovo</span>
+              {/* <span className="truncate text-xs text-muted-foreground">
               Basic Plan
-            </span> */}
-          </div>
-        </SidebarMenuButton>
+              </span> */}
+            </div>
+          </SidebarMenuButton>
+        </Link>
       </SidebarMenuItem>
     </SidebarMenu>
   )
