1aa82e7cdfc37f11fbc00fef94240586690e8b9e
Vishrut Agrawal <zvishrut@gmail.com>
2025-05-25 14:12:40 -0700

    fixed db setup to allow for transactions

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,25 @@
 import 'server-only'
 
-import { drizzle } from 'drizzle-orm/neon-http'
-import { neon } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 import { config } from 'dotenv'
 import * as schema from '@/drizzle/schema'
 
 config({ path: '.env' })
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  pool: Pool | undefined
+}
 
-const sql = neon(process.env.DATABASE_URL!)
+const pool =
+  globalForDb.pool ??
+  new Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
 
-export const db = drizzle(sql, { schema })
+if (process.env.NODE_ENV !== 'production') globalForDb.pool = pool
+
+export const db = drizzle({ client: pool, schema })
