69be2e43d2b21f38990766ca61370e0d5d89a606
Vishrut Agrawal <zvishrut@gmail.com>
2025-05-28 21:58:34 -0700

    Added google auth scopes

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -7,3 +7,7 @@ export const SIGNUP_ROUTE = '/auth/sign-up'
 
 export const FORGOT_PASSWORD_ROUTE = '/auth/forgot-password'
 export const RESET_PASSWORD_ROUTE = '/auth/reset-password'
+
+export enum GoogleScopes {
+  GMAIL_READONLY = 'https://www.googleapis.com/auth/gmail.readonly',
+}
